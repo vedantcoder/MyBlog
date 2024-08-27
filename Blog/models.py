3fc5337a564b68f5e9417abe7421c6f711db63ce
vedantcoder <71256376+vedantcoder@users.noreply.github.com>
2024-08-27 11:49:32 +0530

    Update models.py
    Ckeditor issue fixed

--- Blog/models.py
+++ Blog/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
-from ckeditor.fields import RichTextField
-
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -29,8 +27,7 @@ class Post(models.Model):
     header_image = models.ImageField(null=True, upload_to="images/", blank=True)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = RichTextField(blank=True, null=True)
-    # body = models.TextField()
+    body = models.TextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='testing')
     snippet = models.CharField(max_length=255)
